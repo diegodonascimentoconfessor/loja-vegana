bff200f5e5fbdfdccb1466ee16b2acda43f8362b
Diego Confessor <diegondonascimento@gmail.com>
2024-08-28 16:54:08 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,43 @@
+import { SafeAreaView, StyleSheet, StatusBar, Image, Text, View, FlatList } from 'react-native';
+import {list} from '.';
+import { ProductItem } from './components/produtv-item';
+import { Product } from './types/Product';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar/>
+      <Image 
+        source={require('./assets/hero.webp')}
+        resizeMode='cover'
+        style={styles.hero}
+      />
+      <View style={styles.area}>
+        <Text style={styles.h1}>Produtos</Text>
+        <FlatList
+          data={list}
+          renderItem={({item}: {item:Product})=> (<ProductItem product={item}/>)}
+          keyExtractor={item => item.id.toString()}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  hero:{
+    width: '100%',
+    height: 120
+  },
+  area:{
+    padding: 10,
+    flex: 1
+  },
+  h1:{
+    fontSize: 24,
+    marginBottom: 10
+  },
+});
---
+++ data.ts
@@ -0,0 +1,152 @@
+const list: Produtos[] = [
+    {
+        id: 1,
+        image: "https://www.sadia.com.br/assets/images/_/products/dca974f6f4be28deeda644c8ddc617e76c52bcf1.webp",
+        name: "Mandioca Aipim Macaxeira",
+        price: 20.00,
+    },
+    {
+        id: 2,
+        image: "https://www.sadia.com.br/assets/images/_/products/d9dd0eec5efa6eae92104939a17ff3ecebfd2f4b.webp",
+        name: "Nuggets de Legumes Sadia Veg&Tal",
+        price: 13.90
+    },
+    {
+        id: 3,
+        image: "https://acdn.mitiendanube.com/stores/001/323/634/products/spavegano_sabliq_barra_geldental_amorahibisco_deslavanda__physalis1-bc2c62a04e86ff488316281934513376-640-0.webp",
+        name: "Nuggets Legumes",
+        price: 11.00,
+    },
+    {
+        id: 4,
+        image: "https://m.media-amazon.com/images/I/61DBEfRwb4L._AC_SX679_.jpg",
+        name: "Sora Carne Moída Vegetal Multigrãos Vegano 200G",
+        price: 16.00,
+    },
+    {
+        id: 5,
+        image: "https://m.media-amazon.com/images/I/71-EXUnxEXL.__AC_SX300_SY300_QL70_ML2_.jpg",
+        name: "Konjac Massa MF Macarrão Espaguete Zero Carboidrato",
+        price: 18.89,
+
+    },
+
+    {
+
+        id: 5,
+        image: "https://m.media-amazon.com/images/I/71-EXUnxEXL.__AC_SX300_SY300_QL70_ML2_.jpg",
+        name: "Konjac Massa MF Macarrão Espaguete Zero Carboidrato",
+        price: 18.89,
+
+
+
+
+    },
+    {
+
+        id: 6,
+        image: "https://m.media-amazon.com/images/I/61ORVUgVf9L._AC_SX679_.jpg",
+        name: ,
+        price: 25.90,
+
+
+    },
+    {
+        id: 7,
+        image: "https://m.media-amazon.com/images/I/51dPAULaniL.__AC_SX300_SY300_QL70_ML2_.jpg",
+        name:
+            price: 19.00,
+
+
+    },
+    {
+
+        id: 8,
+        image: "https://m.media-amazon.com/images/I/51dPAULaniL.__AC_SX300_SY300_QL70_ML2_.jpg",
+        name:,
+        price: 19.00,
+
+    },
+    {
+        id: 9,
+        image: "https://m.media-amazon.com/images/I/51dPAULaniL.__AC_SX300_SY300_QL70_ML2_.jpg",
+        name:,
+        price: 19.00,
+    },
+    {
+        id: 10,
+        image:"https://m.media-amazon.com/images/I/71jM8qZW6pL.__AC_SX300_SY300_QL70_ML2_.jpg" ,
+        name:"Mãe Terra Granola Tradicional Zero Açúcares 800 g",
+        price: 27.90,
+},
+
+
+    {
+        id: 11,
+        image: "https://m.media-amazon.com/images/I/51TFWQo3RFL._AC_SX679_.jpg",
+        name: "salchisa vegetal",
+        price: 28.00
+    },
+
+    {
+        id: 12,
+        image: ,
+        name:,
+        price:,
+        },
+
+    {
+        id: 13,
+        image: ,
+        name:,
+        price:
+            },
+    {
+        id: 14,
+        image: ,
+        name:,
+        price:
+                },
+    {
+        id: 15,
+        image: ,
+        name:,
+        price:
+    }, 
+    
+    {
+        id: 16,
+        image: ,
+        name:,
+        price:
+     },
+
+     {
+        id: 17,
+        image: ,
+        name:,
+        price:
+     },
+
+     {
+        id: 18,
+        image: ,
+        name:,
+        price:
+     },
+
+     {
+        id: 19,
+        image: ,
+        name:,
+        price:
+     },
+
+     {
+        id: 20,
+        image: ,
+        name:,
+        price:
+     },
+
+]
